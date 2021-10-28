bc4cda2b808d539b55c7f192c6c65c5560408253
Felpszinho <felpsdev95@gmail.com>
2021-10-27 22:08:39 -0300

    service layer

--- src/main/java/com/spring/curso/Resources/UserResource.java
+++ src/main/java/com/spring/curso/Resources/UserResource.java
@@ -1,20 +1,32 @@
 package com.spring.curso.Resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.spring.curso.entities.User;
+import com.spring.curso.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
+	@Autowired
+	private UserService service;
 
 	@GetMapping
-	public ResponseEntity<User>findall(){
-		User user =new User(1L,"Maria","maria@gami.com","99999999","12345");
-		return  ResponseEntity.ok().body(user);
+	public ResponseEntity<List<User>>findall(){
+		List<User> list=service.findall();
+		return  ResponseEntity.ok().body(list);
 		
+	}
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findbyId(@PathVariable Long id){
+		User obj=service.findbyId(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
--- src/main/java/com/spring/curso/repositories/UserRepository.java
+++ src/main/java/com/spring/curso/repositories/UserRepository.java
@@ -1,10 +1,12 @@
 package com.spring.curso.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.spring.curso.entities.User;
-
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+	
 	
 
 }
